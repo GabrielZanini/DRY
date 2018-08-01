9020f6e4570c3f83cbdb824ec5b1a33ad3077c07
GabrielZanini <gabrielroszan@gmail.com>
2018-07-31 22:48:03 -0300

    Animations

---
+++ Dry/Assets/BreakableItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableItem : MonoBehaviour {
+
+    [SerializeField] GameObject NormalItem;
+    [SerializeField] GameObject BreakedItem;
+
+    void Start ()
+    {
+        NormalItem.SetActive(true);
+        BreakedItem.SetActive(false);
+    }
+
+    public void Break()
+    {
+        NormalItem.SetActive(false);
+        BreakedItem.SetActive(true);
+
+        DisableColliders();
+    }
+
+    void DisableColliders()
+    {
+        Collider collider = GetComponent<Collider>();
+        collider.enabled = false;
+
+        GetComponent<Rigidbody>().velocity += Vector3.up * (-0.01f);
+    }
+}
---
+++ Dry/Assets/PlayerDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDamage : MonoBehaviour {
+
+    public Sprite[] damageSprites;
+    public int currentDamage = 0;
+
+    public Image[] damageGraphics;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (currentDamage < 0)
+        {
+            currentDamage = 0;
+        }
+        else if (currentDamage >= damageSprites.Length)
+        {
+            currentDamage = damageSprites.Length - 1;
+        }
+
+        for (int i=0; i<damageGraphics.Length; i++)
+        {
+            damageGraphics[i].sprite = damageSprites[currentDamage];
+        }
+
+	}
+}
